704770585202d883d9b8a7bb9e8603465acd4bdd
fogalqrn <fogalqrn@gmail.com>
2026-01-17 21:51:44 +0800

    Update script.js

--- script.js
+++ script.js
@@ -7,11 +7,20 @@ items.forEach(i=>obs.observe(i));
 
 // language toggle
 let ar=false;
-document.getElementById('langBtn').onclick=()=>{
+const btn=document.getElementById('langBtn');
+const heroLogo=document.getElementById('heroLogo');
+
+btn.onclick=()=>{
   ar=!ar;
-  document.getElementById('langBtn').innerText=ar?'EN':'AR';
+  btn.innerText=ar?'EN':'AR';
+
+  heroLogo.src=ar
+    ? 'FogAlqrn-color1-proto3ARB-TP.png'
+    : 'FogAlqrn-color1-proto4ENG-TP.png';
+
   document.querySelectorAll('[data-en]').forEach(el=>{
     el.innerText=ar?el.dataset.ar:el.dataset.en;
   });
+
   document.documentElement.dir=ar?'rtl':'ltr';
 };
